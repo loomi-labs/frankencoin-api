1593b824f4221f0be2d3a22da3234207a2ea5ce8
samclassix <samclassix@proton.me>
2026-06-16 17:47:39 +0200

    feat: CHFAU

--- src/modules/prices/prices.mgm.ts
+++ src/modules/prices/prices.mgm.ts
@@ -11,7 +11,6 @@ export const ContractBlacklist: Address[] = [
 	'0xBA0D8F05700466d48A7464Ea27AA316875871A29',
 	'0x64E0c986DfE96a8A22dfDFCDe1B08f953aC3Cb7d',
 	'0xc040EaAA8187276278CAc428149e83Ce6271eFFF',
-	'0xBD4DfC058eb95b8De5ceAF39966A1a70F5556F78',
 ].map((i) => normalizeAddress(i));
 
 export const ContractWhitelist: ERC20Info[] = [
